d01e1e12db8eb7e8857ee625f1fa1a5edf648629
Sachal Mohar <sachal16@uvic.ca>
2026-05-14 13:44:32 -0700

    structure to run()

--- impact-hello/src/test/java/com/impacthello/AppTest.java
+++ impact-hello/src/test/java/com/impacthello/AppTest.java
@@ -9,3 +9,12 @@ import picocli.CommandLine.Command;
     description = "CLI tool built for impact.com job application"
 )
 
+public class App implements Runnable {
+    public void run() {
+        System.out.println("hi");
+    }
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(newApp()).execute(args);
+        System.exit(exitCode);
+    }
+}
