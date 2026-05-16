2d5b16058997e1c059dc00b1eb7569e238c1d22b
Sachal Mohar <sachal16@uvic.ca>
2026-05-16 16:08:06 -0700

    grafiti Ansi font done

--- impact-hello/src/main/java/com/impacthello/App.java
+++ impact-hello/src/main/java/com/impacthello/App.java
@@ -11,7 +11,19 @@ import picocli.CommandLine.Command;
 
 public class App implements Runnable {
     public void run() {
-        System.out.println("hi");
+            System.out.println(CommandLine.Help.Ansi.AUTO.string(
+        "@|red  .__                             __                           |@\n" +
+        "@|red  |__| _____ ___________    _____/  |_    ____  ____   _____  |@\n" +
+        "@|red  |  |/     \\\\____ \\__  \\ _/ ___\\   __\\ _/ ___\\/ __ \\ /     \\ |@\n" +
+        "@|red  |  |  Y Y  \\  |_> > __ \\\\  \\___|  |   \\  \\__(  <_> )  Y Y  \\|@\n" +
+        "@|red  |__|__|_|  /   __(____  /\\___  >__|    \\___  >____/|__|_|  /|@\n" +
+        "@|red            \\/|__|       \\/     \\/            \\/            \\/ |@"
+    ));
+
+    System.out.println(CommandLine.Help.Ansi.AUTO.string(
+        "\n@|bold,white   Built for impact.com — by Sachal Mohar|@\n"
+    ));
+    
     }
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
