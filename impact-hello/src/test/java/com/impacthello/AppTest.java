78245d419b6f707dbf4ef4cc0032cfc4f8a36892
Sachal Mohar <sachal16@uvic.ca>
2026-05-15 20:55:04 -0700

    mvn sucessful build

--- impact-hello/src/test/java/com/impacthello/AppTest.java
+++ impact-hello/src/test/java/com/impacthello/AppTest.java
@@ -1,20 +1,4 @@
 package com.impacthello;
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
 
-@Command(
-    name = "impact-hello",
-    mixinStandardHelpOptions = true,
-    version = "1.0",
-    description = "CLI tool built for impact.com job application"
-)
-
-public class App implements Runnable {
-    public void run() {
-        System.out.println("hi");
-    }
-    public static void main(String[] args) {
-        int exitCode = new CommandLine(new App()).execute(args);
-        System.exit(exitCode);
-    }
+public class AppTest {
 }
